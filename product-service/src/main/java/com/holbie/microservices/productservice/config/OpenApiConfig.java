f8df1cb630fde6caec371fea96d048a34737037a
Nihad Gurbanov <7404@holbertonstudents.com>
2024-07-26 12:14:59 +0400

    open api swagger documentation

---
+++ product-service/src/main/java/com/holbie/microservices/productservice/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.holbie.microservices.productservice.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Product Service API")
+                        .description("REST api for product service")
+                        .version("v1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Spring Documentation")
+                        .url("https://github.com/spring-projects/spring-framework/blob/main/README.md"));
+    }
+}

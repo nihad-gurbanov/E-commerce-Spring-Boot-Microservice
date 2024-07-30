f10390c93ce3f253ab394bc46802fb27d38b3389
Nihad Gurbanov <7404@holbertonstudents.com>
2024-07-30 13:24:01 +0400

    aggregation and restclient

--- api-gateway/src/main/java/com/holbie/dev/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/holbie/dev/gateway/routes/Routes.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
 
@@ -19,6 +21,14 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRouter () {
+        return GatewayRouterFunctions.route("product-service")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> orderServiceRouter () {
         return GatewayRouterFunctions.route("order-service")
                 .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
@@ -26,9 +36,26 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRouter () {
+        return GatewayRouterFunctions.route("order-service")
+                .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+
+    @Bean
     public RouterFunction<ServerResponse> inventoryServiceRouter () {
         return GatewayRouterFunctions.route("inventory-service")
                 .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRouter () {
+        return GatewayRouterFunctions.route("inventory-service")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 }
--- order-service/src/main/java/com/holbie/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/holbie/microservices/order/client/InventoryClient.java
@@ -1,11 +1,9 @@
 package com.holbie.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name = "inventory-service", url = "${inventory.url}")
 public interface InventoryClient {
-    @GetMapping("/api/inventory")
+    @GetExchange("/api/inventory")
     Boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }

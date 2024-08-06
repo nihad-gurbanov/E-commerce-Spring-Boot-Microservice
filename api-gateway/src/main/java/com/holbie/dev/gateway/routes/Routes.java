e3ca73a62dedfa9970493c4ab32c268282d68eb9
Nihad Gurbanov <7404@holbertonstudents.com>
2024-08-06 09:41:24 +0400

    circuit breaker pattern with Resilience4j

--- api-gateway/src/main/java/com/holbie/dev/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/holbie/dev/gateway/routes/Routes.java
@@ -4,41 +4,52 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
 @Configuration
 public class Routes {
 
     @Bean
     public RouterFunction<ServerResponse> productServiceRouter () {
-        return GatewayRouterFunctions.route("product-service")
+        return route("product-service")
                 .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(circuitBreaker("productServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> productServiceSwaggerRouter () {
-        return GatewayRouterFunctions.route("product-service")
-                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+        return route("product-service")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions
+                        .http("http://localhost:8080"))
+                .filter(circuitBreaker("productServiceSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .filter(setPath("/api-docs"))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> orderServiceRouter () {
-        return GatewayRouterFunctions.route("order-service")
+        return route("order-service")
                 .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(circuitBreaker("orderServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> orderServiceSwaggerRouter () {
-        return GatewayRouterFunctions.route("order-service")
-                .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+        return route("order-service")
+                .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), HandlerFunctions
+                        .http("http://localhost:8081"))
+                .filter(circuitBreaker("orderServiceSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .filter(setPath("/api-docs"))
                 .build();
     }
@@ -46,16 +57,27 @@ public class Routes {
 
     @Bean
     public RouterFunction<ServerResponse> inventoryServiceRouter () {
-        return GatewayRouterFunctions.route("inventory-service")
+        return route("inventory-service")
                 .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(circuitBreaker("inventoryServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> inventoryServiceSwaggerRouter () {
-        return GatewayRouterFunctions.route("inventory-service")
-                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+        return route("inventory-service")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), HandlerFunctions
+                        .http("http://localhost:8082"))
+                .filter(circuitBreaker("inventoryServiceSwaggerCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service Unavailable, please try again later"))
                 .build();
     }
 }
--- order-service/src/main/java/com/holbie/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/holbie/microservices/order/client/InventoryClient.java
@@ -1,9 +1,26 @@
 package com.holbie.microservices.order.client;
 
+import groovy.util.logging.Slf4j;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
+
+@Slf4j
 public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     Boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default Boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        log.info("Fallback method for skuCode: {}, quantity: {}, failure reason: {}", skuCode, quantity, throwable.getMessage());
+        return false;
+    }
 }
